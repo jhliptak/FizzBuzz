1b98c5e7260b58914439b3d48045f4680ebe10a3
John Liptak <john@biwabik.net>
2021-02-15 13:51:29 -0700

    Basic FizzBuzz

--- FizzBuzz/src/main/java/dev/boundary/waters/FizzBuzz/MySpringBootApplication.java
+++ FizzBuzz/src/main/java/dev/boundary/waters/FizzBuzz/MySpringBootApplication.java
@@ -6,11 +6,43 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MySpringBootApplication {
 
+	
+	/**
+	 * A canonical FizzBuzz implementation.
+	 * 
+	 * Write a program that prints the numbers from 1 to 100. 
+	 * But for multiples of three print “Fizz” instead of the number.
+	 * For the multiples of five print “Buzz”. 
+	 * For numbers which are multiples of both three and five print “FizzBuzz”.
+	 * 
+	 */
+	public static void fizzBuzz() {
+		
+		for(int i = 1; i < 101; i++) {
+			boolean isFactorThree = ((i % 3) == 0);
+			boolean isFactorFive = ((i % 5) == 0);
+			
+			if(isFactorThree && isFactorFive) {
+				System.out.println("FizzBuzz");
+			} else if(isFactorThree) {
+				System.out.println("Fizz");
+			} else if(isFactorFive) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
+
+		}		
+	}
+	
     /**
      * A main method to start this application.
+     * The spring boot call is commented out for now.
      */
+	
     public static void main(String[] args) {
-        SpringApplication.run(MySpringBootApplication.class, args);
+        fizzBuzz();
+        // SpringApplication.run(MySpringBootApplication.class, args);
     }
 
 }
